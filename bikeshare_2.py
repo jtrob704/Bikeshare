5f4e2c065276ed07d3759e82bdb2817d84dfbb01
J.T. Robinson <Jaittarius@hotmail.com>
2018-05-27 19:26:43 -0400

    Enabled popular times of travel statistics

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -1,6 +1,8 @@
 import time
+import calendar
 import pandas as pd
 import numpy as np
+from click._compat import raw_input
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
@@ -17,13 +19,13 @@ def get_filters():
     """
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
-
+    city = raw_input("Please choose one of the following cities  (Chicago, New York City, or Washington): ").lower()
 
     # get user input for month (all, january, february, ... , june)
-
+    month = raw_input("Please enter the month of the bikeshare data to view(January, February, March, April, May, June or all): ").lower()
 
     # get user input for day of week (all, monday, tuesday, ... sunday)
-
+    day = raw_input("Please enter the day of the week of the bikeshare data to view(Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday or all): ").lower()
 
     print('-'*40)
     return city, month, day
@@ -40,7 +42,28 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
+    df = pd.read_csv("/home/jtrob/Documents/Nanodegree/Data Analyst/Bikeshare/bikeshare-2/"+CITY_DATA[city])
+    
+    # convert the Start Time column to datetime
+    df['Start Time'] = pd.to_datetime(df['Start Time'])
 
+    # extract month and day of week from Start Time to create new columns
+    df['month'] = df['Start Time'].dt.month    
+    df['day_of_week'] = df['Start Time'].dt.weekday_name
+
+    # filter by month if applicable
+    if month != 'all':
+        # use the index of the months list to get the corresponding int
+        months = ['january', 'february', 'march', 'april', 'may', 'june']
+        month = months.index(month) + 1        
+
+        # filter by month to create the new dataframe
+        df = df[df['month'] == month]
+
+    # filter by day of week if applicable
+    if day != 'all':
+        # filter by day of week to create the new dataframe
+        df = df[df['day_of_week'] == day.title()]
 
     return df
 
@@ -52,13 +75,38 @@ def time_stats(df):
     start_time = time.time()
 
     # display the most common month
+    # extract month from the Start Time column to create a month column
+    df['month'] = df['Start Time'].dt.month
+    df['month'] = df['month'].apply(lambda x: calendar.month_name[x])
 
+    # find the most common month
+    common_month = df['month'].mode()[0]
+    
+    print('Most Common month', common_month)
+    
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
 
-    # display the most common day of week
+    # display the most common day of the week
+    # extract day from the Start Time column to create a day column
+    df['day_of_week'] = df['Start Time'].dt.weekday_name
 
+    # find the most common day of the week
+    common_day = df['day_of_week'].mode()[0]
+
+    print('Most Common day', common_day)
+
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)  
 
     # display the most common start hour
+    # extract hour from the Start Time column to create an hour column
+    df['hour'] = df['Start Time'].dt.hour
 
+    # find the most common hour
+    common_hour = df['hour'].mode()[0]
+
+    print('Most Common Start Hour:', common_hour)
 
     print("\nThis took %s seconds." % (time.time() - start_time))
     print('-'*40)
