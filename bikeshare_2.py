c34b59e6343edfe3364b601ba25ae69a629661da
J.T. Robinson <Jaittarius@hotmail.com>
2018-05-28 21:50:39 -0400

    Implemeted user statistics and exception handling code

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -17,6 +17,7 @@ def get_filters():
         (str) month - name of the month to filter by, or "all" to apply no month filter
         (str) day - name of the day of week to filter by, or "all" to apply no day filter
     """
+    
     print('Hello! Let\'s explore some US bikeshare data!')
     # get user input for city (chicago, new york city, washington). HINT: Use a while loop to handle invalid inputs
     city = raw_input("Please choose one of the following cities  (Chicago, New York City, or Washington): ").lower()
@@ -42,30 +43,36 @@ def load_data(city, month, day):
     Returns:
         df - Pandas DataFrame containing city data filtered by month and day
     """
-    df = pd.read_csv("/home/jtrob/Documents/Nanodegree/Data Analyst/Bikeshare/bikeshare-2/"+CITY_DATA[city])
+    try:
+        df = pd.read_csv("/home/jtrob/Documents/Nanodegree/Data Analyst/Bikeshare/bikeshare-2/"+CITY_DATA[city])
     
-    # convert the Start Time column to datetime
-    df['Start Time'] = pd.to_datetime(df['Start Time'])
+        df['city'] = city
+        # convert the Start Time column to datetime
+        df['Start Time'] = pd.to_datetime(df['Start Time'])
 
-    # extract month and day of week from Start Time to create new columns
-    df['month'] = df['Start Time'].dt.month    
-    df['day_of_week'] = df['Start Time'].dt.weekday_name
+        # extract month and day of week from Start Time to create new columns
+        df['month'] = df['Start Time'].dt.month    
+        df['day_of_week'] = df['Start Time'].dt.weekday_name
 
-    # filter by month if applicable
-    if month != 'all':
-        # use the index of the months list to get the corresponding int
-        months = ['january', 'february', 'march', 'april', 'may', 'june']
-        month = months.index(month) + 1        
+        # filter by month if applicable
+        if month != 'all':
+            # use the index of the months list to get the corresponding int
+            months = ['january', 'february', 'march', 'april', 'may', 'june']
+            month = months.index(month) + 1        
 
-        # filter by month to create the new dataframe
-        df = df[df['month'] == month]
+            # filter by month to create the new dataframe
+            df = df[df['month'] == month]
 
-    # filter by day of week if applicable
-    if day != 'all':
-        # filter by day of week to create the new dataframe
-        df = df[df['day_of_week'] == day.title()]
+        # filter by day of week if applicable
+        if day != 'all':
+            # filter by day of week to create the new dataframe
+            df = df[df['day_of_week'] == day.title()]
 
-    return df
+        return df
+
+    except:
+        print('\nInvalid data entered. Please enter data that matches given criteria\n')
+        get_filters()
 
 
 def time_stats(df):
@@ -80,7 +87,7 @@ def time_stats(df):
     # find the most common month
     common_month = df['month'].mode()[0]     
     
-    print('Most Common month', common_month)
+    print('Most common month: ', common_month)
     
     # extract day from the Start Time column to create a day column
     df['day_of_week'] = df['Start Time'].dt.weekday_name
@@ -89,7 +96,7 @@ def time_stats(df):
     common_day = df['day_of_week'].mode()[0]
 
     # display the most common day of the week
-    print('Most Common day', common_day)
+    print('Most common day: ', common_day)
 
     # extract hour from the Start Time column to create an hour column
     df['hour'] = df['Start Time'].dt.hour
@@ -165,16 +172,29 @@ def user_stats(df):
     start_time = time.time()
 
     # Display counts of user types
-
+    user_types = df['User Type'].value_counts()
+    print('\nTotal number of users by type: \n', user_types)
 
     # Display counts of gender
+    if df['city'].all() == 'washington':
+        print('\nGender and birth year statistics are not avaiable for Washington')
+        
+    else:
+        gender = df['Gender'].value_counts()
+        print('\nTotal number of users by gender: \n', gender)   
 
 
-    # Display earliest, most recent, and most common year of birth
+        # Display earliest, most recent, and most common year of birth
+        earliest_year = df['Birth Year'].min()
+        recent_year = df['Birth Year'].max()
+        common_year = df['Birth Year'].value_counts().idxmax()
 
+        print('\nThe earliest birth year of users is: ', int(earliest_year))
+        print('\nThe most recent birth year of users is: ', int(recent_year))
+        print('\nThe most common birth year of users is: ', int(common_year))
 
-    print("\nThis took %s seconds." % (time.time() - start_time))
-    print('-'*40)
+        print("\nThis took %s seconds." % (time.time() - start_time))
+        print('-'*40)
 
 
 def main():
